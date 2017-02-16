67f98d2a62aac49240daab718b6c9c1ddf06edec
daniel <danielzipzip@hotmail.com>
2017-02-16 15:23:14 -0300

    atualização

---
+++ src/br/unipe/java/unidade1/Aula 1/ObjetoJapones/src/ProgramaPrincipal.java
@@ -0,0 +1,11 @@
+public class ProgramaPrincipal{
+	
+	public static void main(String[] args) {
+		
+		ObjetoJapones objeto = new ObjetoJapones();
+		objeto.setNomeObjeto("Sagashii");
+		
+		System.out.println(objeto.getNomeObjeto());
+	}
+}
+		
